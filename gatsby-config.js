9555b4dbd7fd5bba64ac1eaaab4c123b8575f206
Kaare Larsen <me@kaareal.com>
2018-04-18 16:27:23 +0200

    adding getting started guide

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,15 @@ module.exports = {
             }
           },
           {
-            resolve: 'gatsby-remark-images'
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              // maxWidth: 800
+              maxWidth: 1200,
+              linkImagesToOriginal: true
+            }
           }
         ]
       }
