0e726968a399552e9356c799d0702766d83056e1
Kaare Larsen <me@kaareal.com>
2018-10-01 10:38:45 +0200

    fixing stuff

---
+++ src/pages/api-docs.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import styled from 'styled-components';
+
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import Theme from '../components/Theme';
+
+const ApiDocs = styled.main`
+  float: left;
+  width: 55%;
+  margin-left: 20%;
+  padding: 2em 4em;
+
+  @media ${(props) => props.theme.mobile} {
+    width: 100%;
+    margin: 0;
+  }
+`;
+
+function DisableTryItOutPlugin() {
+  return {
+    statePlugins: {
+      spec: {
+        wrapSelectors: {
+          allowTryItOutFor: () => () => false
+        }
+      }
+    }
+  };
+}
+
+function DisableAuthorizePlugin() {
+  return {
+    wrapComponents: {
+      authorizeBtn: () => () => null
+    }
+  };
+}
+
+class Api extends React.Component {
+  componentDidMount() {}
+  render() {
+    const { location } = this.props;
+    const { nav } = this.props.pageContext;
+
+    return (
+      <Theme>
+        <div>
+          <Helmet>
+            <title>&middot; Sixgill </title>
+            <script src="https://cdn.jsdelivr.net/npm/redoc/bundles/redoc.standalone.js">
+              {' '}
+            </script>
+          </Helmet>
+          <Header currentPath={location.pathname} fixed />
+          <redoc spec-url="https://raw.githubusercontent.com/sixgill/sense-api-node/master/openapi.json?token=AAMcXj1yoJcyXw6oKkZAPf9G0wkl0Gljks5buw-twA%3D%3D" />
+        </div>
+      </Theme>
+    );
+  }
+}
+
+export default Api;
