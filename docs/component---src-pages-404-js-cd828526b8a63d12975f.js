2cbcd8e0e02d58fc2920e0ccfb74befa6ede9a2e
Kaare Larsen <me@kaareal.com>
2019-04-17 19:43:16 +0200

    new build

---
+++ docs/component---src-pages-404-js-cd828526b8a63d12975f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{140:function(e,t,n){"use strict";n.r(t);var o=n(0),a=n.n(o);t.default=function(){return a.a.createElement("div",null,a.a.createElement("h1",null,"Woops, something went wrong."),a.a.createElement("p",null,"You just hit a route that doesn't exist!"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-cd828526b8a63d12975f.js.map
